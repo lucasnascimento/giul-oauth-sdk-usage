c45f6d951ebd21ff563439eca13de9573cb651b0
Lucas Nascimento <lucas.nascimento@pearson.com>
2013-12-16 17:13:22 -0200

    Teste de SDK

--- src/test/java/br/com/pearson/giuloauthusage/SdkUsageTest.java
+++ src/test/java/br/com/pearson/giuloauthusage/SdkUsageTest.java
@@ -11,20 +11,22 @@ public class SdkUsageTest {
 
 	@Test
 	public void testDoLogin() {
-		String urlBase = "http://office-sp.dclick.com.br/giul/api";
+		String urlBase = "http://coc.dclick.com.br/giul/api";
+		
 		LoginCall call = new LoginCall(urlBase);
-		String result = call.doLogin("bruno.sales", "admin", "coc");
+		String result = call.doLogin("coc", "admin", "coc");
+
 		System.out.println(result);
 	}
-	
 
 	@Test
-	public void testRequestToken() throws Exception {
-		String urlBase = "http://office-sp.dclick.com.br/giul/api";
+	public void testDoRequestToken() throws Exception {
+		String urlBase = "http://coc.dclick.com.br/giul/api";
+
 		RequestTokenCall call = new RequestTokenCall(urlBase);
-		OAuthToken result = call.call(new OAuthConsumer("consumerKey", "consumerSecret"));
+		OAuthToken result = call.call(new OAuthConsumer("key", "secret"));
+
 		System.out.println(result);
 	}
-
 
 }
