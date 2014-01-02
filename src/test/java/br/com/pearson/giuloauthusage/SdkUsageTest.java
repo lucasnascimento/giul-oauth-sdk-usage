ff76d08a0843bf8e38407e498347addcbcf7031d
Lucas Nascimento <lucas.nascimento@pearson.com>
2014-01-02 10:54:25 -0200

    Usando access token para acesso a API via código.

--- src/test/java/br/com/pearson/giuloauthusage/SdkUsageTest.java
+++ src/test/java/br/com/pearson/giuloauthusage/SdkUsageTest.java
@@ -4,27 +4,49 @@ import org.junit.Test;
 
 import br.com.pearson.oauth.basic.OAuthConsumer;
 import br.com.pearson.oauth.basic.OAuthToken;
-import br.com.pearson.oauth.request.LoginCall;
+import br.com.pearson.oauth.basic.TokenType;
+import br.com.pearson.oauth.request.APICall;
+import br.com.pearson.oauth.request.AccessTokenCall;
 import br.com.pearson.oauth.request.RequestTokenCall;
 
 public class SdkUsageTest {
 
-	@Test
-	public void testDoLogin() {
-		String urlBase = "http://office-sp.dclick.com.br/giul/api";
-		LoginCall call = new LoginCall(urlBase);
-		String result = call.doLogin("bruno.sales", "admin", "coc");
-		System.out.println(result);
-	}
-	
+	private String token = "a96078c9-89f8-423a-9151-76cf71b68828";
+	private String secret = "sRSDq8UdmAUrR6xw%2FU9ZpDwrj6jpt%2F%2F%2BGHlsiCbyQT3McXFaEWg8o2a3rLggvl5lb%2B4uj0OpXnur3T0dydhsO65fW7iUMnjfbkwmsfEZbzM%3D";
 
 	@Test
-	public void testRequestToken() throws Exception {
-		String urlBase = "http://office-sp.dclick.com.br/giul/api";
-		RequestTokenCall call = new RequestTokenCall(urlBase);
-		OAuthToken result = call.call(new OAuthConsumer("consumerKey", "consumerSecret"));
-		System.out.println(result);
+	public void testGerarToken() throws Exception {
+		
+		String urlBase = "http://coc.dclick.com.br/giul/api";
+		OAuthConsumer consumer = new OAuthConsumer("key", "secret");
+
+		RequestTokenCall requestTokenCall = new RequestTokenCall(urlBase);
+		OAuthToken requestToken = requestTokenCall.call(consumer);
+		System.out.println(requestToken.getToken());
+
+		// É necessário entrar no /api-explorer, e autorizar o requestToken
+
+		AccessTokenCall accessTokenCall = new AccessTokenCall(urlBase);
+		OAuthToken accessToken = accessTokenCall.call(consumer, requestToken);
+		System.out.println(accessToken.getToken());
+		System.out.println(accessToken.getSecret());
 	}
 
+	@Test
+	public void testAPIME() throws Exception {
+
+		String urlBase = "http://coc.dclick.com.br/giul/api";
+		APICall apiCall = new APICall(urlBase);
+
+		OAuthConsumer oAuthConsumer = new OAuthConsumer("key", "secret");
+		OAuthToken accessToken = new OAuthToken(token, secret, TokenType.ACCESS);
+
+		System.out.println(
+				apiCall.get("/me", null, oAuthConsumer, accessToken));
+
+		System.out.println(
+				apiCall.get("/me/instituicoes", null, oAuthConsumer, accessToken));
+		
+	}
 
 }
